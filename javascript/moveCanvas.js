60af385cf9c05f6219ff3898a134d552d93e56ee
Vifi5 <117041326+Vif15@users.noreply.github.com>
2024-11-24 17:44:37 -0500

    Delete javascript/moveCanvas.js

--- javascript/moveCanvas.js
+++
@@ -1,5 +0,0 @@
-var main = document.getElementById("myCanvas");
-        var render = main.getContext("2d");
-        main.style.left = "400px";
-        main.style.top = "25px";
-        main.style.position = "absolute";
