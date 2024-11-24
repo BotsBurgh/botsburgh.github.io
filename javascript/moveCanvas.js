5ae13d3c1903550dfceaf075a6bb6e21ba12a6d9
Vifi5 <117041326+Vif15@users.noreply.github.com>
2024-11-24 17:11:25 -0500

    Create moveCanvas.js

---
+++ javascript/moveCanvas.js
@@ -0,0 +1,5 @@
+var main = document.getElementById("myCanvas");
+        var render = main.getContext("2d");
+        main.style.left = "400px";
+        main.style.top = "25px";
+        main.style.position = "absolute";
